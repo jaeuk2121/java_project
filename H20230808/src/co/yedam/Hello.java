6dfe937cb59d7ab460be306f6cda13d0476beba1
jaeuk2121 <141694734+jaeuk2121@users.noreply.github.com>
2023-08-08 11:35:34 +0900

    Update Hello.java

--- H20230808/src/co/yedam/Hello.java
+++ H20230808/src/co/yedam/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요...");
 		System.out.println("remote에 반영하기 전22");
+		System.out.println("깃허브에서 작업");
 	}
 }
