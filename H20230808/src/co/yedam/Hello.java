9e154719c1b2e137def463724b625e533fb091e6
admin <admin@YD5-21>
2023-08-08 11:33:18 +0900

    1

--- H20230808/src/co/yedam/Hello.java
+++ H20230808/src/co/yedam/Hello.java
@@ -3,6 +3,6 @@ package co.yedam;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요...");
-		System.out.println("remote에 반영하기 전1");
+		System.out.println("remote에 반영하기 전22");
 	}
 }
